27a671ca04788909d5fa8849189c3fdc65677fbf
Der-Foehn <33294543+Der-Foehn@users.noreply.github.com>
2018-06-08 18:39:16 +0200

    Update download_pool_scrypticons.sh

--- Scrypts/download_pool_scrypticons.sh
+++ Scrypts/download_pool_scrypticons.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 output "Loading Scrypticons..."
 
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xstratum_restart_script.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/Ubuntu initial Setup script.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xLogOut_rootaccess_disabled.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_start_all.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_stop_all.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xkill-reboot-Stratum-screens.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xrootaccess.sh
-wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xrootaccess.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xstratum_restart_script.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/Ubuntu initial Setup script.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xLogOut_rootaccess_disabled.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_start_all.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_stop_all.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xkill-reboot-Stratum-screens.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xrootaccess.sh
+sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xrootaccess.sh
