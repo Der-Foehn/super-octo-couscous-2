48a95da086bbec59257d41fffca4726b17cdc64b
Der-Foehn <33294543+Der-Foehn@users.noreply.github.com>
2018-06-08 18:41:13 +0200

    Update download_pool_scrypticons.sh

--- Scrypts/download_pool_scrypticons.sh
+++ Scrypts/download_pool_scrypticons.sh
@@ -4,7 +4,6 @@ output "Loading Scrypticons..."
 
 sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xstratum_restart_script.sh
 sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/Ubuntu initial Setup script.sh
-sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xLogOut_rootaccess_disabled.sh
 sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_start_all.sh
 sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xcoind_stop_all.sh
 sudo wget https://github.com/Der-Foehn/super-octo-couscous-2/tree/master/Scrypts/xkill-reboot-Stratum-screens.sh
